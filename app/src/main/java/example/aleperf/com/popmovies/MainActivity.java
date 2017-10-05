ffc63d60076ee0d88b9ebb5a7dd071ccfc76592a
alperf <aleperf@users.noreply.github.com>
2017-10-05 23:24:21 +0200

    move MovieLoader outside MainActivity

--- app/src/main/java/example/aleperf/com/popmovies/MainActivity.java
+++ app/src/main/java/example/aleperf/com/popmovies/MainActivity.java
@@ -1,10 +1,8 @@
 package example.aleperf.com.popmovies;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.app.LoaderManager;
-import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -23,12 +21,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
-import example.aleperf.com.popmovies.utilities.JSONUtils;
-import example.aleperf.com.popmovies.utilities.NetworkUtils;
 
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Movie>>,
@@ -300,58 +294,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         outState.putParcelableArrayList(MOVIE_LIST, ((ArrayList<Movie>) mMovies));
 
     }
-
-
-    public static class MovieLoader extends AsyncTaskLoader<List<Movie>> {
-
-        List<Movie> mMovies;
-        boolean mIsLoading;
-        int mPageToLoad;
-
-        MovieLoader(Context context, boolean isLoading, int pageToLoad) {
-            super(context);
-            mIsLoading = isLoading;
-            mPageToLoad = pageToLoad;
-        }
-
-
-        @Override
-        protected void onStartLoading() {
-
-            if (mMovies != null && !mIsLoading) {
-                deliverResult(mMovies);
-            } else {
-
-                forceLoad();
-            }
-        }
-
-        @Override
-        public List<Movie> loadInBackground() {
-
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-            String querySearch = preferences.getString(getContext().getString(R.string.pref_search_key), getContext().getString(R.string.pref_search_most_pop_value));
-            URL queryURL = NetworkUtils.buildQueryURL(querySearch, mPageToLoad);
-            List<Movie> movies = new ArrayList<>();
-            try {
-                String jsonResponse = NetworkUtils.getResponseFromHttpsUrl(queryURL);
-                movies = JSONUtils.getMovieList(jsonResponse);
-                return movies;
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                return movies;
-            }
-
-        }
-
-        @Override
-        public void deliverResult(List<Movie> data) {
-            mMovies = data;
-            super.deliverResult(data);
-        }
-    }
-
 
 }
 
