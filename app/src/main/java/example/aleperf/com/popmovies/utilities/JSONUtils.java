cdada44975fe03196e415e5ba67054d0de628a11
aleperf <aleperf@users.noreply.github.com>
2017-11-22 10:36:18 +0100

    remove unused classes and obsolete documentation.

--- app/src/main/java/example/aleperf/com/popmovies/utilities/JSONUtils.java
+++ app/src/main/java/example/aleperf/com/popmovies/utilities/JSONUtils.java
@@ -1,7 +1,5 @@
 package example.aleperf.com.popmovies.utilities;
 
-
-import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
 import example.aleperf.com.popmovies.Movie;
@@ -11,29 +9,6 @@ import example.aleperf.com.popmovies.Movie;
  */
 
 public class JSONUtils {
-
-
-    /**
-     * Build a list of movies from a JSON string obtained from a query to TheMovieDb
-     * @param jsonString a JSON string returned by a query to TheMovieDb
-     * @return a list of Movies
-
-     */
-
-    public static List<Movie> getMovieList(String jsonString) {
-
-        Gson gson = new Gson();
-        MovieQuery query = gson.fromJson(jsonString, MovieQuery.class);
-        Integer statusCode = query.getStatusCode();
-        //TheMovieDB sends a status code only in case of error
-        //if there is a status code, the query is invalid
-        if(statusCode != null){
-            return null;
-        }
-
-        return query.getResults();
-    }
-
 
     public class MovieQuery {
         @SerializedName("status_code")
--- app/src/main/java/example/aleperf/com/popmovies/utilities/NetworkUtils.java
+++ app/src/main/java/example/aleperf/com/popmovies/utilities/NetworkUtils.java
@@ -3,38 +3,13 @@ package example.aleperf.com.popmovies.utilities;
 
 import android.net.Uri;
 
-
-
-/**
- * Provides static CONSTANTS and  methods to build URLs for querying the TheMovieDb
- * <p>
- * This class requires a valid API KEY to query TheMovieDb, the API KEY is build from
- * the constant THE_MOVIE_DB_API_KEY in the gradle.properties file.
- * In order to make this class work, include this line of code in your
- * gradle.properties file in the gradle folder,
- * putting a valid API KEY between quotes:
- * THE_MOVIE_DB_API_KEY = "YOUR API KEY HERE"
- * <p>
- * your can request a valid API KEY to query TheMovieDb here:
- * https://www.themoviedb.org/documentation/api
- * <p>
- * If your are sharing this code somewhere, please remember to hide your API KEY and put
- * the build.gradle file in the .gitignore file, or take whatever measures necessary to prevent
- * your API KEY to be shared publicly, as required by the TheMovieDb terms of use.
- */
-
 public class NetworkUtils {
-
-
 
     //constants used to build a URL for requesting an image
     private static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
     private static final String IMAGE_FORMAT_W185 = "w185";
     private static final String IMAGE_FORMAT_W300 = "w300";
     private static final String IMAGE_FORMAT_W500 = "w500";
-
-
-
 
 
     public static Uri buildImageUri(String imagePath) {
